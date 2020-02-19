37ba846360e397c87a2dcbd844edd6617a7da785
max-student <max-student@maxtrain.com>
2020-02-19 15:03:53 -0500

    Build

---
+++ Exceptions/BootcampException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exceptions {
+    public class BootcampException : Exception { //must inherit from an exception when creating an exception
+
+        public BootcampException() : base() { }
+
+        public BootcampException(string message) : base(message) { }
+
+        public BootcampException(string message, Exception innerexception) : base(message, innerexception) { }
+
+
+    }
+}
--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -11,7 +11,19 @@ namespace Exceptions {
             catch(DivideByZeroException ex) {
                 Console.WriteLine(ex.Message);
             }
-            
+
+            try {
+                var e = "ABC";
+                e = null;
+                var len = e.Length;
+            } catch (NullReferenceException n) {
+                var bce = new BootcampException("BootCamp Exception", n);
+                throw bce;
+            } 
+            catch (Exception except) {
+                Console.WriteLine(except.Message);
+            }
+
 
 
         }
