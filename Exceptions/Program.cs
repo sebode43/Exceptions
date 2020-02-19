bdccab35691f773c722578e87c6af31de1895929
max-student <max-student@maxtrain.com>
2020-02-19 14:27:34 -0500

    Add project files.

---
+++ Exceptions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exceptions {
+    class Program {
+        static void Main(string[] args) {
+            var a = 1;
+            var b = 1;
+            try {
+                var c = a / (b - b);
+            } 
+            catch(DivideByZeroException ex) {
+                Console.WriteLine(ex.Message);
+            }
+            
+
+
+        }
+    }
+}
